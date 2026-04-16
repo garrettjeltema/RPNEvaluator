1e45ed3cd5989ebde5d920fd27d252ebc1fad162
Garrett Jeltema <gcjeltema@gmail.com>
2026-04-15 17:38:27 -0700

    Add project files.

---
+++ RPNEvaluator/RPNEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPNEvaluator
+{
+    public class RPNEvaluator
+    {
+        public static int Evaluate(string expr, Dictionary<string, int> vars)
+        {
+            // initialize stack
+            var stack = new Stack<int>();
+
+            // initialize input string into tokens
+            var tokens = expr.Split(' ');
+
+            foreach (var token in tokens)
+            {
+                if (int.TryParse(token, out int num))
+                {
+                    stack.Push(num);
+                }
+                else if (vars != null && vars.ContainsKey(token))
+                {
+                    stack.Push(vars[token]);
+                }
+            }
+        }
+    }
+}
